3128c81c4ca20c6d0ffd16a4029f1d63363d697e
brini <brini.mohamedayechi@gmail.com>
2024-12-15 03:18:29 +0100

    animationPage

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import './homepage.css';
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className='homepage'>
+    <img src='/orbital.png' alt="" className='orbital'/>
+      <div className="left">
+        <h1>SAPHERA CHATBOT</h1>
+        <h2>Your journey with intelligent conversations begins here—explore, interact, and innovate!</h2>
+        <h3>Welcome! I'm Saphera, your AI chatbot, here to assist you with anything you need. Whether you're looking for information, support, 
+          or just a friendly chat, I'm here to help you navigate through your queries and tasks. Let's get started!</h3>
+          <Link to="/dashboard">Get Started here</Link>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
